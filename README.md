720a1575397ba4930638c35dda7a50fed8779f17
Andy Dam <andy-dam@outlook.com>
2025-06-02 23:24:42 -0500

    Changed site

--- README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio Website
-
-This website holds information about myself, my projects, my skills, and how to contact me
--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import "./Footer.css"
-
-const Footer = () => {
-  return (
-    <footer className="footer">Copyright &#169; 2023 Andy Dam. All rights reserved.</footer>
-  )
-}
-
-export default Footer
